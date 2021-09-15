c6ea45c3f31151fc40a77b284384dcc9893b9890
Gabriela Mattos <gabrielamattosde@gmail.com>
2021-09-15 18:25:49 -0300

    Finalização do projeto

--- js/scripts.js
+++ js/scripts.js
@@ -155,4 +155,41 @@ $(document).ready(function() {
 
     }
 
+    // scroll para seções
+    let navBtn = $('.nav-item');
+
+    let bannerSection = $('#mainSlider');
+    let aboutSection = $('#about-area');
+    let servicesSection = $('#services-area');
+    let teamSection = $('#team-area');
+    let portfolioSection = $('#portfolio-area');
+    let contactSection = $('#contact-area');
+
+    let scrollTo = '';
+
+    $(navBtn).click(function() {
+        
+        let btnId= $(this).attr('id');
+
+        console.log(btnId);
+
+        if(btnId == 'about-menu') {
+            scrollTo = aboutSection;
+        }else if(btnId == 'services-menu') {
+            scrollTo = servicesSection;
+        } else if(btnId == 'team-menu') {
+            scrollTo = teamSection;
+        } else if(btnId == 'portfolio-menu') {
+            scrollTo = portfolioSection; 
+        } else if(btnId == 'contact-menu') {
+            scrollTo = contactSection;
+        } else {
+            scrollTo = bannerSection;
+        }
+
+        $([document.documentElement, document.body]).stop().animate({
+            scrollTop: $(scrollTo).offset().top - 70
+        }, 1500);
+    });
+
 });
